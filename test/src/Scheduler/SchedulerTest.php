d701afbd94bdb267793b47d48a335ff5789f36f2
ittech227 <ittech227@gmail.com>
2016-08-04 21:54:58 +0300

    Scheduler's refactoring + test

--- test/src/Scheduler/SchedulerTest.php
+++ test/src/Scheduler/SchedulerTest.php
@@ -2,6 +2,8 @@
 
 namespace zaboy\test\scheduler\Scheduler;
 
+use Xiag\Rql\Parser\Node\Query\ScalarOperator\EqNode;
+use Xiag\Rql\Parser\Query;
 use zaboy\rest\DataStore\DbTable;
 use zaboy\rest\DataStore\Memory;
 use zaboy\scheduler\Ticker\Ticker;
@@ -31,65 +33,153 @@ class SchedulerTest extends \PHPUnit_Framework_TestCase
 //        $this->setFilters();
     }
 
-    protected function setFilters()
-    {
-        $this->filterDs->deleteAll();
-        $filterData = [
-            'rql' => 'in(seconds,(3,8,10,15,20,23,33,41,55,59))',
-            'callback' => 'tick_callback',
-            'active' => 1
-        ];
-        $this->filterDs->create($filterData);
+//    protected function setFilters()
+//    {
+//        $this->filterDs->deleteAll();
+//        $filterData = [
+//            'rql' => 'in(seconds,(3,8,10,15,20,23,33,41,55,59))',
+//            'callback' => 'tick_callback',
+//            'active' => 1
+//        ];
+//        $this->filterDs->create($filterData);
+//
+//        $filterData = [
+//            'rql' => 'in(seconds,(4,9,11,16,21,24,34,42,56))',
+//            'callback' => 'tick_callback',
+//            'active' => 1
+//        ];
+//        $this->filterDs->create($filterData);
+//    }
+//
+//    /**
+//     * @param array $options
+//     * @return Ticker
+//     */
+//    protected function setTicker($options = [])
+//    {
+//        $config = $this->container->get('config')['test_schedule_callback'];
+//        $hopCallback = $this->container->get($config['hop']['callback']);
+//        $tickCallback = $this->container->get($config['tick']['callback']);
+//
+//        // Command line options have higher priority
+//        $options = array_merge($config, $options);
+//        $ticker = new Ticker($tickCallback, $hopCallback, $options);
+//        return $ticker;
+//    }
+//
+//    public function test_countCallingCallback()
+//    {
+//        $this->setTicker()
+//            ->start();
+//        $this->assertEquals(19, $this->log->count());
+//    }
+//
+//    public function test_withStepInQueryLessThanStepFromTicker()
+//    {
+//        $this->setExpectedExceptionRegExp('zaboy\scheduler\Scheduler\SchedulerException');
+//        $filterData = [
+//            'id' => 3,
+//            'rql' => 'and(in(seconds,(4,9,11,16,21,24,34,42,56)),eq(tp_seconds,0))',
+//            'callback' => 'tick_callback',
+//            'active' => 1
+//        ];
+//        $item = $this->filterDs->create($filterData, true);
+//        try {
+//            $this->setTicker()
+//                ->start();
+//        } catch (\Exception $e) {
+//            $exceptionClass = get_class($e);
+//            throw new $exceptionClass($e->getMessage(), $e->getCode());
+//        } finally {
+//            $this->filterDs->delete($item['id']);
+//        }
+//    }
 
-        $filterData = [
-            'rql' => 'in(seconds,(4,9,11,16,21,24,34,42,56))',
-            'callback' => 'tick_callback',
+    public function test_createFilterTask()
+    {
+        /** @var \zaboy\scheduler\Scheduler\Scheduler $scheduler */
+        $scheduler = $this->container->get('scheduler');
+
+        $filterData = $scheduler->create([
+            'rql' => 'eq(tp_seconds,0)',
+            'callback' => function() {
+                return 'ky';
+            },
             'active' => 1
-        ];
-        $this->filterDs->create($filterData);
+        ]);
+        $callbackServiceNameFromDataStore = $this->filterDs->read($filterData['id'])['callback'];
+        $this->assertEquals($filterData['callback'], $callbackServiceNameFromDataStore);
+
+        $filterData = $scheduler->read($filterData['id']);
+        $callback = $filterData['callback'];
+        $this->assertEquals(
+            'ky', $callback()
+        );
     }
 
-    /**
-     * @param array $options
-     * @return Ticker
-     */
-    protected function setTicker($options = [])
-    {
-        $config = $this->container->get('config')['test_schedule_callback'];
-        $hopCallback = $this->container->get($config['hop']['callback']);
-        $tickCallback = $this->container->get($config['tick']['callback']);
 
-        // Command line options have higher priority
-        $options = array_merge($config, $options);
-        $ticker = new Ticker($tickCallback, $hopCallback, $options);
-        return $ticker;
-    }
-
-    public function test_countCallingCallback()
+    public function test_updateFilterTask()
     {
-        $this->setTicker()
-            ->start();
-        $this->assertEquals(19, $this->log->count());
-    }
-
-    public function test_withStepInQueryLessThanStepFromTicker()
-    {
-        $this->setExpectedExceptionRegExp('zaboy\scheduler\Scheduler\SchedulerException');
-        $filterData = [
-            'id' => 3,
-            'rql' => 'and(in(seconds,(4,9,11,16,21,24,34,42,56)),eq(tp_seconds,0))',
-            'callback' => 'tick_callback',
+        /** @var \zaboy\scheduler\Scheduler\Scheduler $scheduler */
+        $scheduler = $this->container->get('scheduler');
+        $filterData = $scheduler->create([
+            'rql' => 'eq(tp_seconds,0)',
+            'callback' => function() {
+                return 'ky';
+            },
             'active' => 1
-        ];
-        $item = $this->filterDs->create($filterData, true);
-        try {
-            $this->setTicker()
-                ->start();
-        } catch (\Exception $e) {
-            $exceptionClass = get_class($e);
-            throw new $exceptionClass($e->getMessage(), $e->getCode());
-        } finally {
-            $this->filterDs->delete($item['id']);
-        }
+        ]);
+
+        $id = $filterData['id'];
+
+        $filterDataAfterUpdate = $scheduler->update([
+            'id' => $id,
+            'rql' => 'eq(seconds,0)'
+        ]);
+
+        $this->assertEquals(
+            $filterData['callback'], $filterDataAfterUpdate['callback']
+        );
+        $this->assertEquals(
+            'eq(seconds,0)', $filterDataAfterUpdate['rql']
+        );
+
+        $scheduler->update([
+            'id' => $id,
+            'callback' => function() {
+                return 'ky-ky';
+            },
+        ]);
+        $filterData = $scheduler->read($filterData['id']);
+        $callback = $filterData['callback'];
+        $this->assertEquals(
+            'ky-ky', $callback()
+        );
+    }
+
+
+    public function test_queryFilterTask()
+    {
+        /** @var \zaboy\scheduler\Scheduler\Scheduler $scheduler */
+        $scheduler = $this->container->get('scheduler');
+        $filterData = $scheduler->create([
+            'rql' => 'eq(tp_seconds,0)',
+            'callback' => function() {
+                return 'ky';
+            },
+            'active' => 1
+        ]);
+        $id = $filterData['id'];
+
+        $query = new Query();
+        $query->setQuery(new EqNode(
+            'id', $id
+        ));
+        $items = $scheduler->query($query);
+        $item = array_shift($items);
+        $callback = $item['callback'];
+        $this->assertEquals(
+            'ky', $callback()
+        );
     }
 }
